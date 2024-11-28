7ef90fda1939359b2953ce817db7d95c071e68a0
Marcus Edwards <marcusstephenedwards@gmail.com>
2024-11-27 23:27:05 -0800

    Debug multiprocessing error

--- process_tomography.py
+++ process_tomography.py
@@ -3,29 +3,31 @@ import qiskit.tools.qcvv.tomography as tomo
 from qiskit.qasm import Qasm
 from qiskit.tools.visualization import plot_state
 
-program = Qasm(filename="./intermediate.qasm").parse()
+if __name__ == '__main__':
 
-qr = QuantumRegister(4)
-cr = ClassicalRegister(4)
-circuit = QuantumCircuit(qr, cr)
+    program = Qasm(filename="./intermediate.qasm").parse()
 
-for node in program.children:
-    if node.__class__.__name__ == 'CustomUnitary':
-        if node.name != 'cx':
-            getattr(circuit, node.name)(qr[node.bitlist.children[0].children[1].value])
-        else:
-            getattr(circuit, node.name)(
-                qr[node.bitlist.children[0].children[1].value],
-                qr[node.bitlist.children[1].children[1].value]
-            )
+    qr = QuantumRegister(4)
+    cr = ClassicalRegister(4)
+    circuit = QuantumCircuit(qr, cr)
 
-tomography_set = tomo.process_tomography_set([0, 1, 2, 3])
-tomography_circuits = tomo.create_tomography_circuits(circuit, qr, cr, tomography_set)
+    for node in program.children:
+        if node.__class__.__name__ == 'CustomUnitary':
+            if node.name != 'cx':
+                getattr(circuit, node.name)(qr[node.bitlist.children[0].children[1].value])
+            else:
+                getattr(circuit, node.name)(
+                    qr[node.bitlist.children[0].children[1].value],
+                    qr[node.bitlist.children[1].children[1].value]
+                )
 
-backend = Aer.get_backend('qasm_simulator')
-tomography_job = execute(tomography_circuits, backend=backend, shots=100)
-tomography_result = tomography_job.result()
-process_data = tomo.tomography_data(tomography_result, circuit.name, tomography_set)
+    tomography_set = tomo.process_tomography_set([0, 1, 2, 3])
+    tomography_circuits = tomo.create_tomography_circuits(circuit, qr, cr, tomography_set)
 
-choi_fit = tomo.fit_tomography_data(process_data, options={'trace': 4})
-plot_state(choi_fit)
+    backend = Aer.get_backend('qasm_simulator')
+    tomography_job = execute(tomography_circuits, backend=backend, shots=100)
+    tomography_result = tomography_job.result()
+    process_data = tomo.tomography_data(tomography_result, circuit.name, tomography_set)
+
+    choi_fit = tomo.fit_tomography_data(process_data, options={'trace': 4})
+    plot_state(choi_fit)
--- test.py
+++ test.py
@@ -1,12 +1,22 @@
 from qiskit import Aer, QuantumCircuit, QuantumRegister, ClassicalRegister, execute
+import qiskit.tools.qcvv.tomography as tomo
+from qiskit.tools.visualization import plot_state
 
-qr = QuantumRegister(4)
-cr = ClassicalRegister(4)
-circuit = QuantumCircuit(qr, cr)
-circuit.x(qr[0])
-circuit.measure(qr[0], cr[0])
+if __name__ == '__main__':
 
-backend = Aer.get_backend('qasm_simulator')
-tomography_job = execute(circuit, backend=backend, shots=100)
-tomography_result = tomography_job.result()
+    qr = QuantumRegister(4)
+    cr = ClassicalRegister(4)
+    circuit = QuantumCircuit(qr, cr)
+    circuit.x(qr[0])
+    circuit.measure(qr[0], cr[0])
 
+    tomography_set = tomo.process_tomography_set([0])
+    tomography_circuits = tomo.create_tomography_circuits(circuit, qr, cr, tomography_set)
+
+    backend = Aer.get_backend('qasm_simulator')
+    tomography_job = execute(tomography_circuits, backend=backend, shots=100)
+    tomography_result = tomography_job.result()
+    process_data = tomo.tomography_data(tomography_result, circuit.name, tomography_set)
+
+    choi_fit = tomo.fit_tomography_data(process_data, options={'trace': 4})
+    plot_state(choi_fit)
